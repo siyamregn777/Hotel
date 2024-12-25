8588ac9e618526179727630ce4b5f217fa888cf3
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2024-12-25 09:06:39 +0300

    chante the video to image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,16 @@ import Middle1 from './Middle1/page';
 import Middle2 from './Middle2/page';
 import Middle3 from './Middle3/page';
 import Comques from './comques/page';
-import Video from 'next-video';
-import video2 from '../../videos/1321208-uhd_3840_2160_30fps.mp4';
+import Image from 'next/image';
+import image1 from '../../public/images/pexels-francesco-ungaro-464401.jpg';
 export default function Home() {
   return (
     <div className={styles.wrapper}>
       <main className={styles.main}>
-        <div className={styles.videoContainer}>
-          <Video src={video2} autoPlay muted loop controls={false} />
+        <div className={styles.imageContainer}>
+          <Image src={image1}
+          alt="imgae1"
+          />
           <div className={styles.overlay}>
             <h1>Welcome to Ethiopia</h1>
             <p>Your comfort is our priority.</p>
