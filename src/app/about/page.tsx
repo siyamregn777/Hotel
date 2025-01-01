7550c322aaaa2d125307d7446065fa2d71c157bb
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-01-02 00:53:03 +0300

    add sample lorem into about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,10 +1,10 @@
 import styles from './abouts.module.css'; // Assuming you have CSS in a separate file
-import image1 from '../../../public/images/pexels-chanwalrus-958546.jpg';
+import image1 from '../../../public/back/pexels-jess-vide-4601423.jpg';
+import image2 from '../../../public/back/pexels-carlos-santos-892832-19064811.jpg';
 import Image from 'next/image';
 export default function AboutUs() {
   return (
     <div>
-     
       <main className={styles.container}>
         <h1>About Us</h1>
         <p>Discover our story, mission, and values.</p>
@@ -18,11 +18,15 @@ export default function AboutUs() {
           />
           <div className={styles.textSection}>
             <h2>Our Story</h2>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
-
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+              Sed do eiusmod tempor incididunt ut labore et dolore magna 
+              aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+               ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
             <h2>Our Mission</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Accusamus perferendis id beatae. Reiciendis laborum cupiditate voluptas atque optio porro numquam! Dolores aperiam vel nesciunt! Odit odio voluptatem nisi porro illo!</p>
-
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+               Accusamus perferendis id beatae. Reiciendis laborum cupiditate
+               voluptas atque optio porro numquam! Dolores aperiam vel nesciunt!
+               Odit odio voluptatem nisi porro illo!</p>
             <h2>Our Values</h2>
             <ul>
               <li>Integrity</li>
@@ -32,8 +36,27 @@ export default function AboutUs() {
             </ul>
           </div>
         </div>
+        <div className={styles.text}>
+            <h1>Our Goal</h1>
+            <p>Lorem ipsum dolor sit amet consectetur.</p>
+          </div>
+        <div className={styles.first}>
+          <div className={styles.text1}>
+            <h2>Our Goal</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+              Nostrum excepturi modi ut deserunt in maiores. Ab molestias 
+              possimus eaque numquam consequatur. Enim, libero rem! Mollitia 
+              excepturi nesciunt incidunt quibusdam repellat?</p>
+          </div>
+          <Image
+            src={image2}
+            alt="image 2"
+            width={300}
+            height={400}
+            className={styles.image2}
+          />
+        </div>
       </main>
-
     </div>
   );
 }
