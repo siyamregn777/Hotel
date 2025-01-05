b4d359848c882afb81b1c377a6262044f9a8f4c0
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-01-06 02:32:31 +0300

    create  adminDashboard

---
+++ src/app/adminDashboard/page.tsx
@@ -0,0 +1,12 @@
+'use client'; // Marking this file as a client component
+
+import styles from './adminDashboard.module.css'; // Assuming you have CSS in a separate file
+
+export default function AdminDashboard() {
+  
+  return (
+    <div className={styles.back}>
+       Welcome admin
+    </div>
+  );
+}
