885157df56bd2329d24257e3d67759e30f730e87
siyamregn77 <yeshidagnasiyamregn@gmail.com>
2025-01-06 02:35:47 +0300

    update mongodb.js using mongoose

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -1,22 +1,20 @@
 import mongoose from 'mongoose';
 
-let isConnected: boolean = false; // Track connection status
+let isConnected: boolean = false;
 
 const connectToDatabase = async (dbName: string) => {
-  if (isConnected) {
-    return; // If already connected, do nothing
-  }
+  if (isConnected) return;
 
-  const uri = process.env.MONGODB_URI || ''; // Get the URI from the environment variable
-  
+  const uri = process.env.MONGODB_URI || '';
+
   if (!uri) {
     throw new Error("Please define the MONGODB_URI environment variable");
   }
-  
+
   try {
-    await mongoose.connect(uri);
-    isConnected = true; // Set the connection status
-    console.log(`MongoDB connected to ${dbName} successfully`);
+    await mongoose.connect(uri, { dbName }); // Connect to the specified database
+    isConnected = true;
+    console.log(`Connected to database: ${dbName}`);
   } catch (error) {
     console.error("MongoDB connection error:", error);
     throw error;
